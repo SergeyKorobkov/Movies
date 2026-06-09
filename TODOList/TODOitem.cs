41ac902742bcf39efe204245b04c476b907c1555
skoro <skoro@TECNO>
2026-06-09 03:04:44 +0300

    Добавьте файлы проекта.

---
+++ TODOList/TODOitem.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TODOList
+{
+	public class TODOitem
+	{
+		public string Title { get; set; }
+		public bool DONE { get; set; }
+		public override bool Equals(object? other)
+		{
+			return this.Title.Equals((other as TODOitem).Title);
+		}
+	}
+}
